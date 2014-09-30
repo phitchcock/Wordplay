f1d01c7f76d5fcc8cbe2551061a481ce9a61ceca
phitchcock <phitchcock@me.com>
2014-09-30 11:01:59 -0700

    added new viewcontroller and viewcontroller class

---
+++ WordPlay/EnterNameViewController.swift
@@ -0,0 +1,39 @@
+//
+//  EnterNameViewController.swift
+//  WordPlay
+//
+//  Created by Peter Hitchcock on 9/30/14.
+//  Copyright (c) 2014 Peter Hitchcock. All rights reserved.
+//
+
+import UIKit
+
+class EnterNameViewController: UIViewController {
+
+    //IBOutlets
+    @IBOutlet weak var nameTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
